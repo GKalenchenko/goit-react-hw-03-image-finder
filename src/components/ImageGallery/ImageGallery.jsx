a92f15bca42ecf448763918a755456ff043aa8fd
Kalenchenko Gleb <107788336+GKalenchenko@users.noreply.github.com>
2023-02-12 09:21:55 +0300

    adds query logic to API

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,11 +1,44 @@
 // import PropTypes from 'prop-types';
-
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+import { Component } from 'react';
+import axios from 'axios';
 
-export const ImageGallery = () => {
-  return (
-    <ul className="gallery">
-      <ImageGalleryItem />
-    </ul>
-  );
-};
+const BASE_URL =
+  'https://pixabay.com/api/?key=32008820-29a82a4a3d033faa63b9c6371&image_type=photo&orientation=horizontal&per_page=12';
+
+export class ImageGallery extends Component {
+  state = {
+    isLoading: false,
+    error: null,
+    response: [],
+  };
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+    try {
+      const response = await axios.get(`${BASE_URL}&q=cat&page=1'`);
+      this.setState({ response: response.data.hits });
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  render() {
+    const { isLoading, error, response } = this.state;
+
+    return (
+      <div>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>Something went wrong please try again</p>}
+        {response.length > 0 && (
+          <ul className="gallery">
+            <h2>Gallery!</h2>
+            <ImageGalleryItem images={response} />
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
